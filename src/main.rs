1d7d9a7039b46b9e4a5ee448deb743cb0b1e1cb9
Lucas Gremont <lucas.gremont@etu.iut-tlse3.fr>
2026-01-28 08:51:29 +0100

    added structs PrivateKey and PublicKey, and function to generate both

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,34 @@
-use num_bigint::{BigUint, RandBigInt, ToBigUint};
+use std::fmt::Display;
+
+use num_bigint::{BigInt, BigUint, RandBigInt, ToBigInt, ToBigUint};
 use num_traits::{Euclid, One};
 
 fn main() {
-    let number = generate_prime_n_bits(2048);
-    println!("{number}");
+    let keys  = generate_keys(4096);
+    println!("{} \n {}", keys.0, keys.1);
+}
+
+pub struct PrivateKey{
+    p: BigUint,
+    q: BigUint,
+    d: BigUint,
+}
+
+impl Display for PrivateKey {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({},{},{})", self.p, self.q, self.d)
+    }
+}
+
+pub struct PublicKey {
+    n: BigUint,
+    e: BigUint,
+}
+
+impl Display for PublicKey {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({},{})", self.n, self.e)
+    }
 }
 
 /*
@@ -23,16 +48,16 @@ fn generate_odd_n_bits(n: &u64) -> BigUint {
 fn miller_rabin_witness(n: &BigUint, a: &BigUint) -> bool {
     let mut s = 0;
     let mut d: BigUint = n - BigUint::one();
-    while d.clone() % &2u32 == BigUint::ZERO {
+    while d.clone() % 2u32 == BigUint::ZERO {
         s += 1;
         d = d.div_euclid(&2.to_biguint().unwrap());
     }
-    let mut x: BigUint = BigUint::modpow(&a, &d, &n);
+    let mut x: BigUint = BigUint::modpow(a, &d, n);
     if x == BigUint::one() || x == n - BigUint::one() {
         return false;
     }
-    for _ in 0..s.clone() - 1 {
-        x = BigUint::modpow(&x, &2.to_biguint().unwrap(), &n);
+    for _ in 0..s - 1 {
+        x = BigUint::modpow(&x, &2.to_biguint().unwrap(), n);
         if x == n - BigUint::one() {
             return false;
         }
@@ -49,7 +74,7 @@ fn miller_rabin_test(n: &BigUint) -> bool {
     for _ in 0..nb_tests {
         let two = 2.to_biguint().unwrap();
         let a = rng.gen_biguint_range(&two, &(n - BigUint::one()));
-        if miller_rabin_witness(&n, &a){
+        if miller_rabin_witness(n, &a){
             return false
         }
     }
@@ -62,6 +87,31 @@ fn generate_prime_n_bits(size: u64) -> BigUint{
         number = generate_odd_n_bits(&size);
     }
     number 
+}
+
+/* 
+ * Generate a tuple of keys this way : (<private key>, <public key>)
+ * Where private key is : (p,q,d) (BitUint, BigUint, BigUint)
+ * and public key is (n,e) (BigUint, BigUint)
+ */
+fn generate_keys(n: u64) -> (PrivateKey, PublicKey) {
+    let p = generate_prime_n_bits(n);
+    let q = generate_prime_n_bits(n);
+    let n = &p*&q;
+    let pub_exp: BigInt = 65537.to_bigint().unwrap();
+    let phi = (&p-BigUint::one()) * (&q-BigUint::one());
+
+    let d = pub_exp.modpow(&(BigInt::ZERO - BigInt::one()), &phi.to_bigint().unwrap()).to_biguint().unwrap();
+    let private_key = PrivateKey{
+        p,
+        q,
+        d,
+    };
+    let public_key = PublicKey {
+        n,
+        e: pub_exp.to_biguint().unwrap(),
+    };
+    (private_key, public_key)
 }
 
 #[cfg(test)]
